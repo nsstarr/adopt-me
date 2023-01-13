a9c2dcee8aa52344a018647b6b1ba410c07a25c0
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-01-13 16:05:38 +0000

    install react router v6 and add a separate page with pets details

---
+++ src/Details.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router-dom'
+
+const Details = () => {
+    const { id } = useParams();
+  return <h2>{ id }</h2>;
+};
+
+export default Details;
