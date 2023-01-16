462ec26d15ca81680c894cc90a52fb120a2e56e1
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-01-16 15:16:25 +0000

    add a class component in Carousel.jsx and render it in Details.jsx. Make the hero image change on Click

---
+++ src/Carousel.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import React from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index, //+ coerces the string into number
+      
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal hero" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            //eslint-disable-next-line
+            <img
+              onClick={this.handleIndexClick}
+              data-index={index}
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
 
 const Details = () => {
@@ -19,6 +20,7 @@ const Details = () => {
 
   return (
     <div className="details">
+    <Carousel images = {pet.images}/>
       <div>
         <h1>{pet.name}</h1>
         <h2>{pet.animal} - {pet.breed} - {pet.city}, {pet.state}
