d3162843e557cdc75be67faa250eadbd05e7a5e4
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-01-18 11:40:34 +0000

    install bootstrap and style pagination

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -14,22 +14,29 @@ const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
   };
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <a onClick={prevPage} href="#">
+    <nav aria-label="Page navigation">
+      <ul className="pagination justify-content-center container-fluid">
+        <li className="page-item">
+          <a className="page-link text-dark" onClick={prevPage} href="#">
             Previous
           </a>
         </li>
         {pageNumbers.map((pgNumber) => (
-          <li key={pgNumber}>
-            <a onClick={() => setCurrentPage(pgNumber)} href="#">
+          <li
+            className={`page-item ${currentPage == pgNumber ? "active" : ""}`}
+            key={pgNumber}
+          >
+            <a
+              onClick={() => setCurrentPage(pgNumber)}
+              className="page-link bg-dark text-light"
+              href="#"
+            >
               {pgNumber}
             </a>
           </li>
         ))}
-        <li>
-          <a onClick={nextPage} href="#">
+        <li className="page-item">
+          <a onClick={nextPage} className="page-link text-dark" href="#">
             Next
           </a>
         </li>
