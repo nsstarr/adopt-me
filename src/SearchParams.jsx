86a4076292fae00b66eb0850f2429cb9d1338cb1
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-01-18 14:42:14 +0000

    add switch theme functionality to toggle dark mode on the form and search results

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import useLocalStorage from "use-local-storage";
 import Results from "./Results";
 import useBreedList from "./useBreedList";
 import fetchSearch from "./fetchSearch";
@@ -18,6 +19,12 @@ const SearchParams = () => {
   const [breeds] = useBreedList(animal);
   const [currentPage, setCurrentPage] = useState(1);
   const [recordsPerPage] = useState(5);
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "search-params"
+  );
+
 
   const results = useQuery(["search", requestParams], fetchSearch);
   const pets = results?.data?.pets ?? [];
@@ -26,10 +33,16 @@ const SearchParams = () => {
   const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
   const currentRecords = pets.slice(indexOfFirstRecord, indexOfLastRecord);
   const nPages = Math.ceil(pets.length / recordsPerPage);
+  
+  const switchTheme = () => {
+    const newTheme = theme === 'search-params' ? 'dark' : 'search-params';
+    setTheme(newTheme)
+  }
 
   return (
     <>
-      <div className="search-params">
+      <div className="search-params" data-theme={theme}>
+      <button onClick={switchTheme}>Switch to {theme === 'search-params' ? 'Dark' : 'Light'} Theme</button>
         <Form
           handleSubmit={(e) => {
             e.preventDefault();
