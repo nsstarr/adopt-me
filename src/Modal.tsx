35eae8c7559bb178bd0103017f4e47acd8bd29c2
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-01-23 15:33:01 +0000

    refactor Modal with Typescript

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, MutableRefObject, ReactElement } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
-  const elRef = useRef(null);
+const Modal = ({ children }: {children: ReactElement}) => {
+  const elRef : MutableRefObject<HTMLDivElement | null> = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
-
-    return () => modalRoot.removeChild(elRef.current)
+    if(elRef.current) {
+      modalRoot.removeChild(elRef.current);
+    }
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
