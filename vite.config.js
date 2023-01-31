9065a2d28b96f9d84dc9de284edb6f119eb988dd
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-01-31 12:31:07 +0000

    install vitest mock to mock a fetch API response in useBreedList.test/jsx

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   root: "src",
   test: {
-    environment: "happy-dom"
+    environment: "happy-dom",
+    setupFiles: ["./setupVitest.js"],
   },
 });
